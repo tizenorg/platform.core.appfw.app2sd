ff3b4dd95c88cc4b74dacfb53c7e79c6f7786f92
jongmyeongko <jongmyeong.ko@samsung.com>
2016-04-05 19:48:56 +0900

    add server/client functions
    test_app2ext usage has been changed.
    now, all the test can be tried to.
    
    install example)
    pre-condition: prepare /tmp/org.example.basicuiapplication-1.0.0-arm.tpk
    1. (tizenglobalapp)$test_app2ext --pre-install
    2. (tizenglobalapp)$pkgcmd -it tpk -p {pkg_path}
    3. (tizenglobalapp)$test_app2ext --post-install
    4. (tizenglobalapp)$test_app2ext --enable (mount external part)
    5. (tizenglobalapp)$test_app2ext --disable (unmount external part)
    
    Change-Id: I8d4d3c07f4632228d7cb15c015012c095a365c4d
    Signed-off-by: jongmyeongko <jongmyeong.ko@samsung.com>

---
+++ plugin/app2sd/inc/app2sd_client_interface.h
@@ -0,0 +1,214 @@
+/*
+ * app2ext
+ *
+ * Copyright (c) 2012 Samsung Electronics Co., Ltd. All rights reserved.
+ *
+ * Contact: Garima Shrivastava<garima.s@samsung.com>
+ *	Jyotsna Dhumale <jyotsna.a@samsung.com>
+ *	Venkatesha Sarpangala <sarpangala.v@samsung.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __APPTOSD_INTERFACE_H__
+#define __APPTOSD_INTERFACE_H__
+
+/**
+ * @file app2sd_client_interface.h
+ * @version 0.2
+ * @brief    This file declares API of app2sd library
+ */
+/**
+ * @addtogroup APPLICATION_FRAMEWORK
+ * @{
+ *
+ * @defgroup app2sd
+ * @version    0.2
+ *
+ * @section    Header to use them:
+ * @code
+ * #include <app2sd_client_interface.h>
+ * @endcode
+ *
+ * @addtogroup app2sd
+ * @{
+ */
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <app2ext_interface.h>
+
+/**
+ * @brief : This API prepares the setup for installation in SD card.
+ *		It should be called before actual installation is done.
+ * @pre			vfat type sd card must be present.
+ * @post		Installation is done by package installer.
+ 		Encryption password is saved in db TZ_SYS_DB/.app2sd.db
+ * @param[in] appname		application package name
+ *				[Ex: com.samsung.calculator]
+ *This entry is parsed from application package control/manifest file.
+ * @param[in] dir_list		directory structure of the application
+ * @param[in] size	size of memory required by application(in MB).
+ *This entry is parsed from application package control/manifest file.
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	None.
+ */
+int app2sd_client_usr_pre_app_install(const char *pkgid,
+		GList* dir_list, int size, uid_t uid);
+int app2sd_client_pre_app_install(const char *pkgid,
+		GList* dir_list, int size);
+
+/**
+ * @brief : This API does post installation operations after
+ *		the installation in SD card
+ * @param[in] appname		application package name
+ *				[Ex: com.samsung.calculator]
+ * @param[in] install_status	Status of installation of package
+ *[ enum app2ext_status].If package installation failed then
+ * install_status= APP2EXT_STATUS_FAILURE else if installation
+ * was successful then install_status = APP2EXT_ISTATUS_SUCCESS.
+ * @pre		Installation should be done by package installer.
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	@see enum app2sd_install_status
+ */
+int app2sd_client_usr_post_app_install(const char *pkgid,
+		app2ext_status install_status, uid_t uid);
+int app2sd_client_post_app_install(const char *pkgid,
+		app2ext_status install_status);
+
+/**
+ * @brief : This API prepares the setup for upgradation of
+ *		 application package
+ * @pre			vfat type sd card must be present.
+ * @post		Upgradation is done by package installer.
+ * @param[in] appname		application package name
+ *				[Ex: com.samsung.calculator]
+ * @param[in] dir_list		directory structure of the application
+ * @param[in] size	size of memory required by application(in MB).
+ *This entry is parsed from application package control/manifest file.
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	None.
+ */
+int app2sd_client_usr_pre_app_upgrade(const char *pkgid,
+		GList* dir_list, int size, uid_t uid);
+int app2sd_client_pre_app_upgrade(const char *pkgid,
+		GList* dir_list, int size);
+
+/**
+ * @brief : This API does post upgradation operations after
+ *		the installation in SD card
+ * @param[in] appname		application package name
+ *				[Ex: com.samsung.calculator]
+ * @param[in] install_status	Status of installation of package
+ *[ enum app2extl_status].If package upgradation failed then
+ * upgrade_status= APP2EXT_STATUS_FAILURE else if upgradation
+ * was successful then upgrade_status = APP2EXT_STATUS_SUCCESS.
+ * @pre		Upgradation should be done by package installer.
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	@see enum app2ext_status
+ */
+int app2sd_client_usr_post_app_upgrade(const char *pkgid,
+		app2ext_status upgrade_status, uid_t uid);
+int app2sd_client_post_app_upgrade(const char *pkgid,
+		app2ext_status upgrade_status);
+/**
+ * @brief: This API prepares the setup for uninstallation
+ * @pre			Package must be installed in sdcard.
+ * @post	Package is uninstalled by the package installer.
+ * @param[in] appname		application package name
+ *				[Ex: com.samsung.calculator]
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	None.
+ */
+int app2sd_client_usr_pre_app_uninstall(const char *pkgid, uid_t uid);
+int app2sd_client_pre_app_uninstall(const char *pkgid);
+
+/**
+ * @brief This API removes the resources created during
+ app2sd setup.It is called after uninstallation.
+ * @pre			Package must be uninstalled .
+ * @post		Encryption password is removed from sqlite db.
+ * @param[in] appname		application package name
+ *				[Ex: com.samsung.calculator]
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	None.
+ */
+int app2sd_client_usr_post_app_uninstall(const char *pkgid, uid_t uid);
+int app2sd_client_post_app_uninstall(const char *pkgid);
+
+/**
+ * @brief : This API moves the package from sd card
+ to internal memory and vice versa.
+ * @param[in] pkgid		application package id
+ *				[Ex: com.samsung.calculator]
+ * @param[in] move_type		Move type[enum app2ext_move_type]
+ *			[sd card to internal/internal to sd card]
+ * @param[in] dir_list		directory structure of the application
+ * @pre			Package must be installed and its installation
+ * location should be known. Use dedicated API to get installation location.
+ * @post	Package is moved to new location.
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	None.
+ */
+int app2sd_client_usr_move_installed_app(const char *pkgid,
+		GList* dir_list, app2ext_move_type move_type, uid_t uid);
+int app2sd_client_move_installed_app(const char *pkgid,
+		GList* dir_list, app2ext_move_type move_type);
+
+/**
+ * @brief : This API Enables the application in sd card
+ for use. This API should be called by AUL.
+ * @param[in] pkgid		application package id
+*				[Ex: com.samsung.calculator]
+ * @pre			Package must be installed
+ * @post	application is enabled in SD card.
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	None.
+ */
+int app2sd_client_usr_on_demand_setup_init(const char *pkgid, uid_t uid);
+int app2sd_client_on_demand_setup_init(const char *pkgid);
+
+/**
+ * @brief : This API Disables the application in sd card
+ . This API should be called by Launchpad callback which will be registered
+  during app launch for exit action of the application
+ * @param[in] pkgid		application package id
+*				[Ex: com.samsung.calculator]
+ * @pre			Package must be installed and enabled
+ *			and application must be running in SD card
+ * @post	application is disabked in SD card.
+ * @return	0 if success,  error code(>0) if fail
+ * @remark	None.
+ */
+int app2sd_client_usr_on_demand_setup_exit(const char *pkgid, uid_t uid);
+int app2sd_client_on_demand_setup_exit(const char *pkgid);
+
+int app2sd_client_usr_force_clean(const char *pkgid, uid_t uid);
+int app2sd_client_force_clean(const char *pkgid);
+
+/**
+ * @brief : This is the plug-in load function.
+ 	  The plugin has to bind its functions to function pointers of storage handle
+ * @param[in/out] st_interface		Specifies the storage interface.
+ * @return	None
+*/
+API void app2ext_on_load(app2ext_interface *st_interface);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
--- plugin/app2sd/src/app2sd_internals_registry.c
+++ plugin/app2sd/src/app2sd_internals_registry.c
@@ -34,8 +34,6 @@
 #include <dlog.h>
 #include <time.h>
 #include <db-util.h>
-
-/* For multi-user support */
 #include <tzplatform_config.h>
 
 #define MAX_QUERY_LEN 4096
@@ -52,11 +50,6 @@ sqlite3 *app2sd_db;
 	 password text\
 	)"
 
-/*
- *@_app2sd_initialize_db
- *This function is to initialize sqlite db.
- * return: On success, it will return zero else  if fail then return val<0.
- */
 int _app2sd_initialize_db()
 {
 	char *error_message = NULL;
@@ -65,103 +58,81 @@ int _app2sd_initialize_db()
 	fp = fopen(APP2SD_DB_FILE, "r");
 	if (fp != NULL) {
 		fclose(fp);
-		ret =
-		    db_util_open(APP2SD_DB_FILE, &app2sd_db,
-				 DB_UTIL_REGISTER_HOOK_METHOD);
+		ret = db_util_open(APP2SD_DB_FILE, &app2sd_db,
+			DB_UTIL_REGISTER_HOOK_METHOD);
 
 		if (ret != SQLITE_OK) {
-			app2ext_print("====>>>> connect menu_db [%s] failed!\n",
-				     APP2SD_DB_FILE);
+			_E("connect menu_db [%s] failed",
+				APP2SD_DB_FILE);
 			return -1;
 		}
 		return 0;
 	}
 
-	ret =
-	    db_util_open(APP2SD_DB_FILE, &app2sd_db,
-			 DB_UTIL_REGISTER_HOOK_METHOD);
+	ret = db_util_open(APP2SD_DB_FILE, &app2sd_db,
+		 DB_UTIL_REGISTER_HOOK_METHOD);
 
 	if (ret != SQLITE_OK) {
-		app2ext_print("====>>>> connect menu_db [%s] failed!\n",
-			     APP2SD_DB_FILE);
+		_E("connect menu_db [%s] failed",
+			APP2SD_DB_FILE);
 		return -1;
 	}
 
-	if (SQLITE_OK !=
-	    sqlite3_exec(app2sd_db, QUERY_CREATE_TABLE_APP2SD,
-			 NULL, NULL, &error_message)) {
-		app2ext_print("Don't execute query = %s, "
-			     "error message = %s\n",
-			     QUERY_CREATE_TABLE_APP2SD, error_message);
+	if (SQLITE_OK != sqlite3_exec(app2sd_db,
+		QUERY_CREATE_TABLE_APP2SD, NULL, NULL,
+		&error_message)) {
+		_E("don't execute query = (%s), " \
+			"error message = (%s)",
+			QUERY_CREATE_TABLE_APP2SD, error_message);
 		return -1;
 	}
 
-	app2ext_print("\n db_initialize_done ");
+	_D("db_initialize_done");
+
 	return 0;
 }
 
-/*
- *@_app2sd_set_password_in_db
- *This function is to store password into  db.
- * param[in]: pkgid: package id
- * param[in]: password: password string
- * return: On success, it will return 0.
- * Else appropriate error will be returned.
- */
 int _app2sd_set_password_in_db(const char *pkgid,
 				      const char *passwd)
 {
 	char *error_message = NULL;
 
-	char *query = sqlite3_mprintf("insert into app2sd(pkgid,password) values (%Q, %Q)", pkgid, passwd);
+	char *query = sqlite3_mprintf("insert into app2sd(pkgid,password) values (%Q, %Q)",
+		pkgid, passwd);
 
 	if (SQLITE_OK != sqlite3_exec(app2sd_db, query, NULL, NULL,
-				      &error_message)) {
-		app2ext_print("Don't execute query = %s, error message = %s\n",
-			     query, error_message);
-
+		&error_message)) {
+		_E("don't execute query = (%s), error message = (%s)",
+			query, error_message);
 		sqlite3_free(query);
 		return APP2EXT_ERROR_SQLITE_REGISTRY;
 	}
 	sqlite3_free(query);
-	app2ext_print("\n sqlite insertion done ");
+	_D("sqlite insertion done");
+
 	return APP2EXT_SUCCESS;
 }
 
-/*
- *@_app2sd_remove_password_from_db
- *This function is to remove passwod from  db.
- * param[in]: pkgid: package id
- * return: On success, it will return 0.
- * Else appropriate error will be returned.
- */
 int _app2sd_remove_password_from_db(const char *pkgid)
 {
 	char *error_message = NULL;
-
-	char *query = sqlite3_mprintf("delete from app2sd where pkgid LIKE %Q", pkgid);
-	app2ext_print("\n deletion querys is %s ", query);
+	char *query = sqlite3_mprintf("delete from app2sd"
+		" where pkgid LIKE %Q", pkgid);
+	_D("deletion querys is (%s)", query);
 
 	if (SQLITE_OK != sqlite3_exec(app2sd_db, query, NULL,
-				      NULL, &error_message)) {
-		app2ext_print("Don't execute query = %s, error message = %s\n",
-			     query, error_message);
+		NULL, &error_message)) {
+		_E("don't execute query = (%s), "
+			"error message = (%s)", query, error_message);
 		sqlite3_free(query);
 		return APP2EXT_ERROR_SQLITE_REGISTRY;
 	}
 
 	sqlite3_free(query);
-	app2ext_print("\n app2sd password deletion done ");
-	return APP2EXT_SUCCESS;
 
+	return APP2EXT_SUCCESS;
 }
 
-/*
- *@_app2sd_get_password_from_db
- *This function is to retrive password from DB
- * param[in]: pkgid: package id
- * return: On success, it will return the password, else NULL.
- */
 char *_app2sd_get_password_from_db(const char *pkgid)
 {
 	char query[MAX_QUERY_LEN] = { 0 };
@@ -171,46 +142,49 @@ char *_app2sd_get_password_from_db(const char *pkgid)
 	char *passwd = NULL;
 
 	sqlite3_snprintf(MAX_QUERY_LEN, query,
-		 "select * from app2sd where pkgid LIKE '%s'", pkgid);
-	app2ext_print("\n access querys is %s ", query);
+		"select * from app2sd where pkgid LIKE '%s'", pkgid);
+	_D("access querys is (%s)", query);
 
 	if (SQLITE_OK != sqlite3_prepare(app2sd_db, query,
-					 strlen(query), &stmt, &tail)) {
-		app2ext_print("sqlite3_prepare error\n");
+		strlen(query), &stmt, &tail)) {
+		_E("sqlite3_prepare error");
+
 		return NULL;
 	}
 
 	rc = sqlite3_step(stmt);
 	if (rc != SQLITE_ROW || rc == SQLITE_DONE) {
-		app2ext_print("No records found");
+		_E("no records found");
 		goto FINISH_OFF;
 	}
 	passwd = malloc(PASSWORD_LENGTH + 1);
 	if (passwd == NULL) {
-		app2ext_print("memory allocation failed\n");
+		_E("memory allocation failed");
 		goto FINISH_OFF;
 	}
 
-	app2ext_print("\n entry available in sqlite");
+	_D("entry available in sqlite");
 	strncpy(passwd, (const char*)sqlite3_column_text(stmt, 1),
 		PASSWORD_LENGTH);
 	if (passwd == NULL) {
-		app2ext_print("\n password is NULL ");
+		_E("data is NULL");
 		goto FINISH_OFF;
 	}
-	app2ext_print("\n passwd is %s ", passwd);
 	if (SQLITE_OK != sqlite3_finalize(stmt)) {
-		app2ext_print("error : sqlite3_finalize\n");
+		_E("error : sqlite3_finalize");
 		goto FINISH_OFF;
 	}
+
 	return passwd;
 
 FINISH_OFF:
 	rc = sqlite3_finalize(stmt);
 	if (rc != SQLITE_OK) {
-		app2ext_print(" sqlite3_finalize failed - %d", rc);
+		_E("sqlite3_finalize failed(%d)", rc);
 	}
+
 	if (passwd)
 		free(passwd);
+
 	return NULL;
 }
