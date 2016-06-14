455d0bd0ff96170f88c6ceb234eacb1c470b038a
jongmyeongko <jongmyeong.ko@samsung.com>
2016-06-14 19:05:03 +0900

    add enalbe/disable function for entire packages.
    Change-Id: I8434dc46662ba15380d884eca3f0b0c149355c0f
    Signed-off-by: jongmyeongko <jongmyeong.ko@samsung.com>

--- plugin/app2sd/src/app2sd_internals_registry.c
+++ plugin/app2sd/src/app2sd_internals_registry.c
@@ -36,10 +36,9 @@
 /*sqlite  db code*/
 #define APP2SD_DB_FILE tzplatform_mkpath(TZ_SYS_DB, ".app2sd.db")
 sqlite3 *app2sd_db;
-#define QUERY_CREATE_TABLE_APP2SD "create table app2sd \
-	(pkgid text primary key,\
-	 password text\
-	)"
+#define QUERY_CREATE_TABLE_APP2SD "CREATE TABLE IF NOT EXISTS app2sd_info " \
+	"(pkgid TEXT PRIMARY KEY NOT NULL, password TEXT NOT NULL, " \
+	"filename TEXT NOT NULL, uid INTEGER)"
 
 int _app2sd_initialize_db()
 {
@@ -82,17 +81,66 @@ int _app2sd_initialize_db()
 	return 0;
 }
 
-int _app2sd_set_password_in_db(const char *pkgid, const char *passwd)
+static int _app2sd_check_existing_info(const char *pkgid,
+		const char *loopback_device, uid_t uid)
+{
+	char *query = NULL;
+	const char *val = NULL;
+	sqlite3_stmt *stmt = NULL;
+	int ret = 0;
+
+	query = sqlite3_mprintf("select count(*) from app2sd_info " \
+		"where pkgid=%Q and filename=%Q and uid=%d",
+		pkgid, loopback_device, uid);
+
+	ret = sqlite3_prepare_v2(app2sd_db, query, strlen(query), &stmt, NULL);
+	if (ret != SQLITE_OK) {
+		_E("prepare failed (%s)", sqlite3_errmsg(app2sd_db));
+		sqlite3_free(query);
+		return -1;
+	}
+
+	if (sqlite3_step(stmt) != SQLITE_ROW) {
+		_E("failed to step");
+		sqlite3_finalize(stmt);
+		return -1;
+	}
+
+	val = (const char *)sqlite3_column_text(stmt, 0);
+	ret = atoi(val);
+	sqlite3_finalize(stmt);
+
+	return ret;
+}
+
+int _app2sd_set_password_in_db(const char *pkgid, const char *passwd,
+		const char *loopback_device, uid_t uid)
 {
 	char *error_message = NULL;
 	char *query = NULL;
+	int ret = 0;
 
-	query = sqlite3_mprintf("insert into app2sd" \
-		"(pkgid, password) values (%Q, %Q)", pkgid, passwd);
+	ret = _app2sd_check_existing_info(pkgid, loopback_device, uid);
+	if (ret < 0) {
+		_E("failed to get existing info");
+		return APP2EXT_ERROR_SQLITE_REGISTRY;
+	}
 
-	if (SQLITE_OK != sqlite3_exec(app2sd_db, query, NULL, NULL,
-		&error_message)) {
-		_E("don't execute query = (%s), error message = (%s)",
+	if (ret == 0) {
+		_D("insert item");
+		query = sqlite3_mprintf("insert into app2sd_info " \
+			"(pkgid, password, filename, uid) values (%Q, %Q, %Q, %d)",
+			pkgid, passwd, loopback_device, uid);
+	} else {
+		_D("update item");
+		query = sqlite3_mprintf("update app2sd_info " \
+			"set password=%Q where pkgid=%Q, loopback_device=%Q, uid=%d",
+			passwd, pkgid, loopback_device, uid);
+	}
+
+	ret = sqlite3_exec(app2sd_db, query, NULL, NULL, &error_message);
+	if (ret != SQLITE_OK) {
+		_E("failed to execute query(%s), error message(%s)",
 			query, error_message);
 		sqlite3_free(query);
 		return APP2EXT_ERROR_SQLITE_REGISTRY;
@@ -102,48 +150,106 @@ int _app2sd_set_password_in_db(const char *pkgid, const char *passwd)
 	return APP2EXT_SUCCESS;
 }
 
-int _app2sd_remove_password_from_db(const char *pkgid)
+int _app2sd_remove_password_from_db(const char *pkgid, uid_t uid)
 {
 	char *error_message = NULL;
 	char *query = NULL;
+	int ret = 0;
 
-	query = sqlite3_mprintf("delete from app2sd" \
-		" where pkgid=%Q", pkgid);
+	query = sqlite3_mprintf("delete from app2sd_info " \
+		"where pkgid=%Q and uid=%d", pkgid, uid);
 
-	if (SQLITE_OK != sqlite3_exec(app2sd_db, query, NULL,
-		NULL, &error_message)) {
-		_E("don't execute query = (%s), "
-			"error message = (%s)", query, error_message);
+	ret = sqlite3_exec(app2sd_db, query, NULL, NULL, &error_message);
+	if (ret != SQLITE_OK) {
+		_E("failed to execute query(%s), error message(%s)",
+			query, error_message);
 		sqlite3_free(query);
 		return APP2EXT_ERROR_SQLITE_REGISTRY;
 	}
-
 	sqlite3_free(query);
 
 	return APP2EXT_SUCCESS;
 }
 
-char *_app2sd_get_password_from_db(const char *pkgid)
+int _app2sd_get_info_from_db(const char *filename, char **pkgid, uid_t *uid)
+{
+	char *query = NULL;
+	sqlite3_stmt *stmt = NULL;
+	int ret = APP2EXT_SUCCESS;
+
+	_D("filename(%s)", filename);
+	query = sqlite3_mprintf("select * from app2sd_info " \
+		"where filename=%Q", filename);
+
+	ret = sqlite3_prepare_v2(app2sd_db, query, strlen(query), &stmt, NULL);
+	if (ret != SQLITE_OK) {
+		_E("prepare failed (%s)", sqlite3_errmsg(app2sd_db));
+		sqlite3_free(query);
+		return APP2EXT_ERROR_SQLITE_REGISTRY;
+	}
+
+	ret = sqlite3_step(stmt);
+	if (ret != SQLITE_ROW || ret == SQLITE_DONE) {
+		_W("no records found");
+		ret = APP2EXT_SUCCESS;
+		goto FINISH_OFF;
+	}
+
+	*pkgid = strdup((const char*)sqlite3_column_text(stmt, 0));
+	if (*pkgid == NULL) {
+		_E("out of memory");
+		ret = APP2EXT_ERROR_SQLITE_REGISTRY;
+		goto FINISH_OFF;
+	}
+
+	*uid = sqlite3_column_int(stmt, 3);
+	if (*uid != GLOBAL_USER && *uid < REGULAR_USER) {
+		_E("invalid uid");
+		ret = APP2EXT_ERROR_SQLITE_REGISTRY;
+		goto FINISH_OFF;
+	}
+
+	if (SQLITE_OK != sqlite3_finalize(stmt)) {
+		_E("error : sqlite3_finalize");
+		ret = APP2EXT_ERROR_SQLITE_REGISTRY;
+		goto FINISH_OFF;
+	}
+	sqlite3_free(query);
+
+	return APP2EXT_SUCCESS;
+
+FINISH_OFF:
+	if (*pkgid) {
+		free(*pkgid);
+		*pkgid = NULL;
+	}
+
+	sqlite3_finalize(stmt);
+	sqlite3_free(query);
+
+	return ret;
+}
+
+char *_app2sd_get_password_from_db(const char *pkgid, uid_t uid)
 {
 	char *query = NULL;
 	char *passwd = NULL;
-	const char *tail = NULL;
 	sqlite3_stmt *stmt = NULL;
-	int rc = 0;
+	int ret = 0;
 
-	query = sqlite3_mprintf("select * from app2sd" \
-		" where pkgid=%Q", pkgid);
+	query = sqlite3_mprintf("select * from app2sd_info " \
+		"where pkgid=%Q and uid=%d", pkgid, uid);
 
-	if (SQLITE_OK != sqlite3_prepare(app2sd_db, query,
-		strlen(query), &stmt, &tail)) {
-		_E("sqlite3_prepare error");
+	ret = sqlite3_prepare_v2(app2sd_db, query, strlen(query), &stmt, NULL);
+	if (ret != SQLITE_OK) {
+		_E("prepare failed (%s)", sqlite3_errmsg(app2sd_db));
 		sqlite3_free(query);
 		return NULL;
 	}
 
-	rc = sqlite3_step(stmt);
-	if (rc != SQLITE_ROW || rc == SQLITE_DONE) {
-		_E("no records found");
+	ret = sqlite3_step(stmt);
+	if (ret != SQLITE_ROW || ret == SQLITE_DONE) {
+		_W("no records found");
 		goto FINISH_OFF;
 	}
 	passwd = malloc(PASSWORD_LENGTH + 1);
@@ -167,14 +273,11 @@ char *_app2sd_get_password_from_db(const char *pkgid)
 	return passwd;
 
 FINISH_OFF:
-	rc = sqlite3_finalize(stmt);
-	if (rc != SQLITE_OK) {
-		_E("sqlite3_finalize failed(%d)", rc);
-	}
-	sqlite3_free(query);
-
 	if (passwd)
 		free(passwd);
+
+	sqlite3_finalize(stmt);
+	sqlite3_free(query);
 
 	return NULL;
 }
