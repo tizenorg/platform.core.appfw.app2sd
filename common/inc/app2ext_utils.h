ba72a4458eca1347afe35a7444321d5b10d5f3c4
jongmyeongko <jongmyeong.ko@samsung.com>
2016-07-18 18:11:24 +0900

    get mount path of sdcard using storage api instead of hard-coded path.
    Change-Id: I37061efb63b430f771e8aa2fd00218675c9f429f
    Signed-off-by: jongmyeongko <jongmyeong.ko@samsung.com>

--- common/inc/app2ext_utils.h
+++ common/inc/app2ext_utils.h
@@ -36,6 +36,7 @@ extern "C" {
 #include <glib.h>
 #include <sys/stat.h>
 #include <tzplatform_config.h>
+#include <storage-internal.h>
 
 #define _E(fmt, arg...) LOGE(fmt, ##arg)
 #define _D(fmt, arg...) LOGD(fmt, ##arg)
@@ -53,8 +54,7 @@ extern "C" {
 #define OWNER_ROOT 0
 #define REGULAR_USER 5000
 #define GLOBAL_USER tzplatform_getuid(TZ_SYS_GLOBALAPP_USER)
-#define MMC_PATH tzplatform_mkpath(TZ_SYS_MEDIA, "SDCardA1")
-#define APP2SD_PATH tzplatform_mkpath(TZ_SYS_MEDIA, "SDCardA1/app2sd")
+#define EXTIMG_DIR "app2sd"
 
 #define APP2SD_BUS_NAME "org.tizen.app2sd"
 #define APP2SD_OBJECT_PATH "/org/tizen/app2sd"
